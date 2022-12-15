2a6259c240bb0e90650656cb169b09200182f145
imrohitroy23 <90301368+imrohitroy23@users.noreply.github.com>
2022-12-15 19:36:23 +0530

    added 15 December code

---
+++ 15Dec.java
@@ -0,0 +1,60 @@
+class 15Dec {
+    static String BalancedString(int N) {
+          // code here
+          StringBuilder ans = new StringBuilder();
+          StringBuilder allCh = new StringBuilder();
+  
+          for(int i=0; i<26;i++)
+          {
+              allCh.append((char)('a'+i));
+          }
+  
+          int allChTimes = N/26;
+          int r = N%26;
+  
+          //s2
+          while(allChTimes-->0)
+          {
+              ans.append(allCh);
+          }
+  
+          //s3
+          if(N%2==0)
+          {
+              for(int i=0; i<r/2; i++) 
+              ans.append((char)('a'+i));
+  
+              for(int i=26-r/2 ;i<26; i++)
+               ans.append((char)('a'+i));
+          }
+  
+          //s4
+          else
+          {
+              //sod
+              int sod =0; int t=N;
+              while(t>0)
+              {
+                  sod += t%10; t/=10;
+              }
+              if(sod%2==0)
+              {
+                  for(int i=0; i<(r+1)/2; i++) 
+                  ans.append((char)('a'+i));
+  
+                  for(int i=26-(r-1)/2 ;i<26; i++)
+                   ans.append((char)('a'+i));
+              }
+              else //sod is odd
+              {
+                  for(int i=0; i<(r-1)/2; i++) 
+                  ans.append((char)('a'+i));
+  
+                  for(int i=26-(r+1)/2 ;i<26; i++)
+                   ans.append((char)('a'+i));
+  
+              }
+          }
+          return ans.toString();
+      }
+  }
