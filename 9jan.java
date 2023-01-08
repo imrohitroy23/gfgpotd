09dbdff22c31808d50af75e3d13239c7f7b151e0
imrohitroy23 <90301368+imrohitroy23@users.noreply.github.com>
2023-01-09 02:42:11 +0530

    updated 9jan answer

--- 9jan.java
+++ 9jan.java
@@ -1,13 +1,18 @@
 class 9jan{
     int solve(int N, int[] A) {
         for (int i = N; i > 0; i--) {
-        A[i - 1]++;
-        if (A[i - 1] > 9) {
-            A[i - 1] = 0;
-        } else {
-            return i;
+            A[i - 1]++;
+            if (A[i - 1] > 9) {
+                A[i - 1] = 0;
+                // Check if we reached the first bowl
+                if (i == 1) {
+                    return 0;
+                }
+            } else {
+                return i;
+            }
         }
+        return -1;
     }
-    return -1;
-    }
+    
 }
