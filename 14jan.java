02751786fc514e1cfa3f5dbad483ebb4849920ef
imrohitroy23 <90301368+imrohitroy23@users.noreply.github.com>
2023-01-14 16:29:37 +0530

    updated

--- 14jan.java
+++ 14jan.java
@@ -1,99 +1,101 @@
-public class 14jan {
+
+
+class 14jan {
+
     ArrayList<Integer> maximumToys(int N, int A[], int Q, ArrayList<Integer> Queries[]){
 
-        int[][] a = new int[N][2];
+         int[][] a = new int[N][2];
 
-       for(int i=0;i<N;i++) {
+        for(int i=0;i<N;i++) {
 
-           a[i][0]=A[i];
+            a[i][0]=A[i];
 
-           a[i][1]=i;
+            a[i][1]=i;
 
-       }
+        }
 
-       Arrays.sort(a,(b,c) -> b[0]-c[0]);
+        Arrays.sort(a,(b,c) -> b[0]-c[0]);
 
-       
+        
 
-       long[] prefix_sum = new long[N];
+        long[] prefix_sum = new long[N];
 
-       long s=0;
+        long s=0;
 
-       for(int i=0;i<N;i++) {
+        for(int i=0;i<N;i++) {
 
-           A[a[i][1]]=i;
+            A[a[i][1]]=i;
 
-           s+=(long)(a[i][0]);
+            s+=(long)(a[i][0]);
 
-           prefix_sum[i]=s;
+            prefix_sum[i]=s;
 
-       }
+        }
 
-       
+        
 
-       ArrayList<Integer> ans = new ArrayList<>();
+        ArrayList<Integer> ans = new ArrayList<>();
 
-       for(ArrayList<Integer> querie : Queries) {
+        for(ArrayList<Integer> querie : Queries) {
 
-           ans.add(find(querie,prefix_sum,A,a));
+            ans.add(find(querie,prefix_sum,A,a));
 
-       }
+        }
 
-       return ans;
+        return ans;
 
-   }
+    }
 
-   // TC :- O(k.log(n))
+    // TC :- O(k.log(n))
 
-   int find(ArrayList<Integer> q,long[] prefix_sum,int[] A,int[][] a) {
+    int find(ArrayList<Integer> q,long[] prefix_sum,int[] A,int[][] a) {
 
-       int lo=0,hi=prefix_sum.length-1,ans=0,c=q.get(0),k=q.get(1);
+        int lo=0,hi=prefix_sum.length-1,ans=0,c=q.get(0),k=q.get(1);
 
-       int mid,count_of_excluded;
+        int mid,count_of_excluded;
 
-       long actualSum;
+        long actualSum;
 
-       while(lo<=hi) {
+        while(lo<=hi) {
 
-           mid = lo+(hi-lo)/2;
+            mid = lo+(hi-lo)/2;
 
-           actualSum=prefix_sum[mid];
+            actualSum=prefix_sum[mid];
 
-           count_of_excluded=0;
+            count_of_excluded=0;
 
-           
+            
 
-           for(int i=0;i<k;i++) {
+            for(int i=0;i<k;i++) {
 
-               if(mid>=A[q.get(i+2)-1]) {
+                if(mid>=A[q.get(i+2)-1]) {
 
-                   actualSum-=(long)a[A[q.get(i+2)-1]][0];
+                    actualSum-=(long)a[A[q.get(i+2)-1]][0];
 
-                   count_of_excluded++;
+                    count_of_excluded++;
 
-               }
+                }
 
-           }
+            }
 
-           
+            
 
-           if(actualSum<=c) {
+            if(actualSum<=c) {
 
-               lo=mid+1;
+                lo=mid+1;
 
-               ans = Math.max(ans,mid-count_of_excluded+1);
+                ans = Math.max(ans,mid-count_of_excluded+1);
 
-           } else {
+            } else {
 
-               hi=mid-1;
+                hi=mid-1;
 
-           }
+            }
 
-       }
+        }
 
-       return ans;
+        return ans;
 
-   }
-
+    }
 
 }
