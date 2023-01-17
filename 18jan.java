ffd88d5731db0cc98ceba6f88a84eea5dfc054bd
imrohitroy23 <90301368+imrohitroy23@users.noreply.github.com>
2023-01-18 00:04:07 +0530

    added 18jan

---
+++ 18jan.java
@@ -0,0 +1,25 @@
+public class 18jan {
+
+    //Function to find first node if the linked list has a loop.
+    public static int findFirstNode(Node head){
+        //code here\
+        Node f=head;
+        Node s=head;
+        Node t=head;
+        while(f.next!=null && f.next.next!=null ){
+            f=f.next.next;
+            s=s.next;
+            if(s==f){
+                while(s!=t){
+                    s=s.next;
+                    t=t.next;
+                }
+            return s.data;
+                
+            }
+            
+        }
+        return -1;
+    }
+
+}
