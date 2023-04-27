359d2b051119abd0543ed9534f011b192021824b
imrohitroy23 <90301368+imrohitroy23@users.noreply.github.com>
2023-04-27 21:06:20 +0530

    added

---
+++ 27apr.java
@@ -0,0 +1,29 @@
+public class 27apr {
+    public List<TreeNode> generateTrees(int n) {
+        if (n == 0) {
+            return new ArrayList<>();
+        }
+        return generateTrees(1, n);
+    }
+
+    private List<TreeNode> generateTrees(int start, int end) {
+        List<TreeNode> result = new ArrayList<>();
+        if (start > end) {
+            result.add(null);
+            return result;
+        }
+        for (int i = start; i <= end; i++) {
+            List<TreeNode> leftSubtrees = generateTrees(start, i - 1);
+            List<TreeNode> rightSubtrees = generateTrees(i + 1, end);
+            for (TreeNode left : leftSubtrees) {
+                for (TreeNode right : rightSubtrees) {
+                    TreeNode root = new TreeNode(i);
+                    root.left = left;
+                    root.right = right;
+                    result.add(root);
+                }
+            }
+        }
+        return result;
+    }
+}
