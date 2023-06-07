09b229a1d4931a0d31e71e7fd91e72fd6013ed23
imrohitroy23 <90301368+imrohitroy23@users.noreply.github.com>
2023-06-07 23:17:43 +0530

    commit

---
+++ 06jun.java
@@ -0,0 +1,19 @@
+public class 06jun {
+    public int minFlips(int a, int b, int c) {
+        int ans=0;
+        while(a!=0 || b!=0 || c!=0){
+            int x1=a&1;
+            int x2=b&1;
+            int c3=c&1;
+            if((x1|x2)!=c3) if((x1&x2)==1) ans+=2;
+            else ans+=1;
+            
+        
+        a>>=1;
+        b>>=1;
+        c>>=1;
+        }
+        return ans;
+    }
+    
+}
